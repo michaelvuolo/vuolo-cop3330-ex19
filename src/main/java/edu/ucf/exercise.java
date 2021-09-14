98e9c826b88f41e858976f72e99ee76e3d95d495
michaelvuolo <michaelvuolo1@gmail.com>
2021-09-13 21:07:51 -0400

    Update exercise.java

--- src/main/java/edu/ucf/exercise.java
+++ src/main/java/edu/ucf/exercise.java
@@ -9,6 +9,24 @@ import java.util.Scanner;
 
 public class exercise {
     public static void main(String[] args) {
-        
+        // create new scanner
+        Scanner input = new Scanner(System.in);
+
+        // record values
+        System.out.print("Enter your weight (lbs): ");
+        float weight = input.nextFloat();
+        System.out.print("Enter your height (in.): ");
+        float height = input.nextFloat();
+
+        // calculate BMI
+        float bmi = (weight / (height * height)) * 703;
+
+        // output based on bmi
+        if (bmi >= 18.5 && bmi <= 25)
+            System.out.print("Your BMI is " + String.format("%.1f", bmi) + ".\nYou are within the ideal weight range.");
+        else if (bmi > 25)
+            System.out.print("Your BMI is " + String.format("%.1f", bmi) + ".\nYou are overweight. You should see your doctor.");
+        else if (bmi < 18.5)
+            System.out.print("Your BMI is " + String.format("%.1f", bmi) + ".\nYou are underweight. You should see your doctor.");
     }
 }
